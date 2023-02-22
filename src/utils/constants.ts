b2e9627d37c817d193a31daa25dbe89536ffb949
manologonz <manologonz203@gmail.com>
2023-02-21 21:00:34 -0600

    changing mysql for postgresql

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,26 +6,25 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 
-export const MYSQL_PORT = Number(process.env.MYSQL_PORT) || 3306;
+export const DB_PORT = Number(process.env.DB_PORT) || 5432;
 
-export const MYSQL_HOST = process.env.MYSQL_HOST || "localhost";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USERNAME = process.env.DB_USERNAME || "postgres";
 
-export const MYSQL_USERNAME = process.env.MYSQL_USERNAME || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "";
-
-export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || "mydb";
+export const DB_DATABASE = process.env.DB_DATABASE || "api_starter";
 
 export const APP_DOMAIN = process.env.APP_DOMAIN || "localhost";
 
 const apiPath = path.join(__dirname, "..", "api");
 export const CONNECTION_CONFIGS: DataSourceOptions = {
-    type: "mysql",
-    host: MYSQL_HOST,
-    port: MYSQL_PORT,
-    username: MYSQL_USERNAME,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
+    type: "postgres",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
     logging: true,
     synchronize: true,
     entities: [
